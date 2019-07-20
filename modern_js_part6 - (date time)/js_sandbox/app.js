93f64389e97ef5805ae6e730d5de56b74101967d
Rajeev-Gill <rajeev.sgill@gmail.com>
2019-07-20 14:58:02 +0100

    datetime

---
+++ modern_js_part6 - (date time)/js_sandbox/app.js
@@ -0,0 +1,17 @@
+let val;
+
+const today = new Date();
+const bday = new Date("11-17-1996"); //mm-dd-yyyy
+
+//Get month number
+val = today.getMonth();
+
+//Get Day of month
+val = today.getDate();
+
+//day Number i.e 6 for Sat
+val = today.getDay();
+
+//Didnt finish tutorial vid go back for more if needed: 
+
+console.log(val);
