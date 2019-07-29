d9b5d7d70918f32e530ac8b9eb87c69b8b93ab1b
Rajeev-Gill <34521865+Rajeev-Gill@users.noreply.github.com>
2019-07-29 22:08:20 +0100

    Update app.js

--- basic_js_part1 - (console, data types and type conversion)/js_sandbox/app.js
+++ basic_js_part1 - (console, data types and type conversion)/js_sandbox/app.js
@@ -31,6 +31,24 @@ var person = {
   }
 };
 
+//Object Constructor Function
+// Constructor function for Person objects
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+  this.nationality = "English";
+}
+
+// Create 2 Person objects
+var myFather = new Person("John", "Doe", 50, "blue");
+var myMother = new Person("Sally", "Rally", 48, "green");
+
+// Display nationality
+document.getElementById("demo").innerHTML =
+"My father is " + myFather.nationality + ". My mother is " + myMother.nationality; 
+
 //you can still add properties to constant objects using dot notation
 
 //console.log(person);
